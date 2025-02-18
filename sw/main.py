4a089116591e129c6f347b6d05ce4aac4a456c94
Beat Besmer <beat.besmer@gmail.com>
2025-02-18 09:23:24 +0100

    Test for buttons
    see #1

--- sw/main.py
+++ sw/main.py
@@ -16,6 +16,10 @@ s2 = Stepper(HALF_STEP, Pin(18, Pin.OUT), Pin( 5, Pin.OUT), Pin(17, Pin.OUT), Pi
 s3 = Stepper(HALF_STEP, Pin( 4, Pin.OUT), Pin( 2, Pin.OUT), Pin(15, Pin.OUT), Pin(32, Pin.OUT), delay=.003 )
 s4 = Stepper(HALF_STEP, Pin(33, Pin.OUT), Pin(25, Pin.OUT), Pin(26, Pin.OUT), Pin(27, Pin.OUT), delay=.003 )
 
+# Buttons
+mode = Pin(0, Pin.IN)
+button_a = Pin(14, Pin.IN)
+button_b = Pin(12, Pin.IN)
 
 # Hall sensors
 h1 = Pin(36,Pin.IN)
@@ -23,8 +27,9 @@ h2 = Pin(39,Pin.IN)
 h3 = Pin(34,Pin.IN)
 h4 = Pin(35,Pin.IN)
 
-sensors = [h1,h2,h3,h4]
-steppers = [s1,s2,s3,s4]
+sensors = [h1, h2, h3, h4]
+steppers = [s1, s2, s3, s4]
+buttons = [mode, button_a, button_b]
 
 mode = Pin(0, Pin.IN)
 
@@ -50,6 +55,11 @@ runner = Driver()
 
 
 while True:
+    
+    for button in buttons:
+        if button() == 0:
+            print(f"Button number {buttons.index(button)} pressed")
+    
     for sensor in sensors:
         if sensor() == 4:
             index = sensors.index(sensor)
