851e99089a35d8eef8b1f480eb99b4e86974980d
Beat Besmer <beat.besmer@gmail.com>
2025-03-01 23:06:57 +0100

    fine tune the calibration process for the digits

--- sw/digit.py
+++ sw/digit.py
@@ -3,34 +3,48 @@ import time
 HALL_ACTIVE = 0
 
 class Digit():
-    def __init__(self, stepper, sensor, labels = [], offset = 0, direction = 1):
+    def __init__(self, stepper, sensor, labels = [], offset = 0, direction = 1, label =''):
         self.stepper = stepper
         self.sensor = sensor
         self.labels = labels
         self.direction = direction
-        scelf.position = -1
+        self.position = -1
         self.offset = offset
+        self.label = label
 
     def calibrate(self):
+        print(f"Calibrating the {self.label} digit")
         print(f"{self.labels}")
 
+        if self.sensor() == HALL_ACTIVE:
+            self.position = 0 
+            while self.sensor() == HALL_ACTIVE:
+                self.stepper.step(1, -self.direction)
+                self.position += 1
+            print(f"moved {self.position} out of the magnet area")
+
+        self.position = 0
         print(f"Starting calibration")
         while self.sensor() != HALL_ACTIVE:
             self.stepper.step(1, self.direction)
-            self.position = 2
+            self.position +=1
             
-        time.sleep(.5)        
+        print(f"Found the magnet after {self.position} steps")
+        self.position = 0
         print(f"Starting at position {self.position}")
         while self.sensor() == HALL_ACTIVE:
             self.position += 1
             self.stepper.step(1,self.direction)
-            print(f"fine tune {self.position}")
+        print(f"Reached end of hall sensor at {self.position}")
         
-        print("Go to center")
-        self.stepper.step(int(self.position /2), -self.direction)
+        print("Go to center of magnet")
+        self.stepper.step(int(self.position / 2), -self.direction)
+        self.position = 0
         print("Go to offset")
         self.stepper.step(self.offset-5, self.direction)
+        time.sleep(0.5)
         self.stepper.step(5, self.direction)
+        self.positon = self.offset
         print(f"calibration ended at position {self.position}")
                 
 
@@ -40,11 +54,11 @@ if __name__ == '__main__':
 
     s1 = Stepper(HALF_STEP, Pin( 4, Pin.OUT), Pin( 2, Pin.OUT), Pin(15, Pin.OUT), Pin(32, Pin.OUT), d)
     hall1 = Pin(34, Pin.IN)
-    d1 = Digit(s1,hall1, ['Mo','Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'],46,1)
+    d1 = Digit(s1,hall1, ['Mo','Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'],32,1, label='Weekdays')
     d1.calibrate()
     
     s2 = Stepper(HALF_STEP, Pin(33, Pin.OUT), Pin(25, Pin.OUT), Pin(26, Pin.OUT), Pin(27, Pin.OUT), d)
     hall2 = Pin(35,Pin.IN)
-    d2 = Digit(s2,hall2, [0,1,2,3,4,5,6,7,8,9],490,-1)
+    d2 = Digit(s2,hall2, [1,2,3,4,5,6,7,8,9,0],33,-1, label='Days')
     d2.calibrate()
                   
