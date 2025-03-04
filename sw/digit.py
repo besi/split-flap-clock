4ee127f102bae10158cb544d671eb0d75847a403
Beat Besmer <beat.besmer@gmail.com>
2025-03-04 18:09:32 +0100

    show the time at startup

--- sw/digit.py
+++ sw/digit.py
@@ -76,28 +76,3 @@ class Digit():
         self.stepper.step(5, self.direction)
         self.positon = self.offset
         print(f"calibration ended at position {self.position}")
-    
-
-if __name__ == '__main__':
-    from machine import Pin
-    d = 0.001
-
-    s1 = Stepper(HALF_STEP, Pin(23, Pin.OUT), Pin(22, Pin.OUT), Pin(21, Pin.OUT), Pin(19, Pin.OUT), d)
-    h1 = Pin(36, Pin.IN)
-    d1 = Digit(s1, h1, ['Mo','Di', 'Mi', 'Do', 'Fr', 'Sa', 'So', '' ,'' ,''], 35, 1, label='Weekdays')
-    d1.calibrate()
-
-    s2 = Stepper(HALF_STEP, Pin(18, Pin.OUT), Pin( 5, Pin.OUT), Pin(17, Pin.OUT), Pin(16, Pin.OUT), d)
-    h2 = Pin(39,Pin.IN)
-    d2 = Digit(s2, h2, [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 35, 1, label='Days10')
-    d2.calibrate()
-
-    s3 = Stepper(HALF_STEP, Pin( 4, Pin.OUT), Pin( 2, Pin.OUT), Pin(15, Pin.OUT), Pin(32, Pin.OUT), d)
-    h4 = Pin(34, Pin.IN)
-    d3 = Digit(s3, h3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 25, -1, label='Days')
-    d3.calibrate()
-
-    s4 = Stepper(HALF_STEP, Pin(33, Pin.OUT), Pin(25, Pin.OUT), Pin(26, Pin.OUT), Pin(27, Pin.OUT), d)
-    h4 = Pin(35, Pin.IN)
-    d4 = Digit(s4, h4, [1,2,3,4,5,6,7,8,9,10,11,12], 480, -1, label='Months')
-    d4.calibrate()
