cb03f9c1c1e66f330195c00b82cc484b56232a90
Beat Besmer <beat.besmer@gmail.com>
2025-03-18 15:16:02 +0100

    Show the current day after calibration of each digit

--- sw/main.py
+++ sw/main.py
@@ -10,29 +10,35 @@ from machine import Pin
 
 d = 0.001
 
+(year,month,mday,h,m,s,weekday,yearday) = utime.localtime()
+
 c1 = 40
 s1 = Stepper(HALF_STEP, Pin(23, Pin.OUT), Pin(22, Pin.OUT), Pin(21, Pin.OUT), Pin(19, Pin.OUT), d)
 h1 = Pin(36, Pin.IN)
 d1 = Digit(s1, h1, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], c1, 1, label='Weekdays')
 d1.calibrate()
+d1.show(weekday)
 
 c2 = 50
 s2 = Stepper(HALF_STEP, Pin(18, Pin.OUT), Pin(5, Pin.OUT), Pin(17, Pin.OUT), Pin(16, Pin.OUT), d)
 h2 = Pin(39,Pin.IN)
 d2 = Digit(s2, h2, [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], c2, 1, label='Days10')
 d2.calibrate()
+d2.show(int(mday/10))
 
-c3 = 25
+c3 = 22
 s3 = Stepper(HALF_STEP, Pin(4, Pin.OUT), Pin(2, Pin.OUT), Pin(15, Pin.OUT), Pin(32, Pin.OUT), d)
 h3 = Pin(34, Pin.IN)
 d3 = Digit(s3, h3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], c3, -1, label='Days')
 d3.calibrate()
+d3.show(mday%10)
 
 c4 = 490
 s4 = Stepper(HALF_STEP, Pin(33, Pin.OUT), Pin(25, Pin.OUT), Pin(26, Pin.OUT), Pin(27, Pin.OUT), d)
 h4 = Pin(35, Pin.IN)
 d4 = Digit(s4, h4, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], c4, -1, label='Months')
 d4.calibrate()
+d4.show(month)
 
 
 # Buttons
