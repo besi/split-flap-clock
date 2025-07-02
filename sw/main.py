98d72ba64beddcc3817d122c8fc071136452447e
Beat Besmer <beat.besmer@gmail.com>
2025-07-02 23:18:36 +0200

    Allow to show both the time or the date

--- sw/main.py
+++ sw/main.py
@@ -33,10 +33,10 @@ s4 = Stepper(HALF_STEP, Pin(1, Pin.OUT), Pin(2, Pin.OUT), Pin(42, Pin.OUT), Pin(
 h4 = Pin(48, Pin.IN)
 d4 = Digit(s4, h4, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], c4, 1, label='Months')
 
-d1.calibrate(); d1.show(BLANK)
-d2.calibrate(); d2.show(BLANK)
-d3.calibrate(); d3.show(BLANK)
-d4.calibrate(); d4.show(month)
+d1.calibrate();
+d2.calibrate();
+d3.calibrate();
+d4.calibrate();
 
 # Buttons
 mode = Pin(0, Pin.IN)
@@ -55,6 +55,17 @@ np = neopixel.NeoPixel(machine.Pin(13),2)
 #np.write()
 
 def showTime():
+    (year,month,mday,h,m,s,weekday,yearday) = time.localtime()
+    hour = int(h/10)
+    if hour == 0: 
+        d1.show(BLANK)
+    else:
+        d1.show(hour)
+    d2.show(h%10)
+    d3.show(int(m/10))
+    d4.show(m%10)
+
+def showDate():
     (year,month,mday,h,m,s,weekday,yearday) = time.localtime()
     d1.show(weekday)
     tens = int(mday/10)
